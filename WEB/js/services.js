bffa19b26db1ea0eaf68fca701e3bbfb339d05d2
Oleksandr <lysenko86@i.ua>
2017-09-19 20:33:59 +0300

    WEB - optimization of code

--- WEB/js/services.js
+++ WEB/js/services.js
@@ -35,14 +35,9 @@ moneyApp.service('requestServ', function($http, localStorageService){
         let url = config.api + '?token=' + token;
         if (method === 'get'){
             url += '&action=' + action;
-            if ("from" in data && "count" in data){
-                url += '&from=' + data.from + '&count=' + data.count;
-                delete data.from;
-                delete data.count;
-            }
-            if ("id" in data){
-                url += '&id=' + data.id;
-                delete data.id;
+            for (let key in data){
+                url += '&'+key+'=' + data[key];
+                delete data[key];
             }
             $http.get(url)
                 .success(function(data){ cb(data); })
@@ -62,217 +57,94 @@ moneyApp.service('requestServ', function($http, localStorageService){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-moneyApp.service('usersServ', function($http, localStorageService){
-    var token = localStorageService.get('token');
+moneyApp.service('usersServ', function(requestServ){
     this.getCount = function(cb){
-        $http.get(config.api + '?action=getCountUsers&token=' + token)
-		.success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('get', 'getCountUsers', {}, cb);
     }
     this.signin = function(user, cb){
-        $http.post(config.api, {
-           action: 'signin',
-           email: user.email,
-           password: user.password
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('post', 'signin', {
+            email:    user.email,
+            password: user.password
+        }, cb);
     }
     this.signup = function(user, cb){
-        $http.post(config.api, {
-           action: 'signup',
-           email: user.email,
-           password: user.password,
-            agree: user.agree
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('post', 'signup', {
+            email:    user.email,
+            password: user.password,
+            agree:    user.agree
+        }, cb);
     }
     this.logout = function(cb){
-        $http.get(config.api + '?action=logout&token=' + token)
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('get', 'logout', {}, cb);
     }
     this.confirm = function(confirm, cb){
-        $http.get(config.api + '?action=confirmEmail&confirm='+confirm[0]+'.'+confirm[1])
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('get', 'confirmEmail', {
+            confirm: confirm[0]+'.'+confirm[1]
+        }, cb);
     }
     this.reset = function(reset, cb){
-        $http.get(config.api + '?action=resetPassword&reset='+reset[0]+'.'+reset[1])
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('get', 'resetPassword', {
+            reset: reset[0]+'.'+reset[1]
+        }, cb);
     }
     this.sendConfirmMail = function(email, cb){
-        $http.post(config.api, {
-           action: 'sendConfirmMail',
-           email: email
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('post', 'sendConfirmMail', {
+            email: email
+        }, cb);
     }
     this.sendPasswordMail = function(email, cb){
-        $http.post(config.api, {
-           action: 'sendPasswordMail',
-           email: email
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('post', 'sendPasswordMail', {
+            email: email
+        }, cb);
     }
     this.getProfile = function(cb){
-        $http.get(config.api + '?action=getProfile&token=' + token)
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('get', 'getProfile', {}, cb);
     }
     this.editPassword = function(chPassword, cb){
-        $http.post(config.api + '?token=' + token, {
-           action: 'editPassword',
-           password: chPassword.password,
-           newPassword: chPassword.newPassword,
+        requestServ.sendRequest('post', 'editPassword', {
+            password:        chPassword.password,
+            newPassword:     chPassword.newPassword,
             confirmPassword: chPassword.confirmPassword
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        }, cb);
     }
     this.removeAccount = function(cb){
-        $http.post(config.api + '?token=' + token, {
-           action: 'removeAccount'
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('post', 'removeAccount', {}, cb);
     }
 });
 
 
 
-moneyApp.service('forumServ', function($http, localStorageService){
-    var token = localStorageService.get('token');
-   this.getPosts = function(from, count, cb){
-        $http.get(config.api + '?action=getPosts&token=' + token + '&from=' + from + '&count=' + count)
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+moneyApp.service('forumServ', function(requestServ){
+    this.getPosts = function(from, count, cb){
+        requestServ.sendRequest('get', 'getPosts', {
+            from:  from,
+            count: count
+        }, cb);
     }
     this.getPost = function(id, cb){
-        $http.get(config.api + '?action=getPost&token=' + token + '&id=' + id)
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('get', 'getPost', {
+            id: id
+        }, cb);
     }
     this.addPost = function(post, cb){
-       $http.post(config.api + '?token=' + token, {
-           action: 'addPost',
-           title: post.title,
-           category: post.category,
-           comment: post.comment
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
-   }
+        requestServ.sendRequest('post', 'addPost', {
+            title:    post.title,
+			category: post.category,
+			comment:  post.comment
+        }, cb);
+	}
     this.addComment = function(fid, comment, cb){
-       $http.post(config.api + '?token=' + token, {
-           action: 'addComment',
-            fid: fid,
-           comment: comment
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
-   }
+        requestServ.sendRequest('post', 'addComment', {
+            fid:     fid,
+			comment: comment
+        }, cb);
+	}
     this.setPostStatus = function(id, status, cb){
-       $http.post(config.api + '?token=' + token, {
-           action: 'setPostStatus',
-           id: id,
-           status: status
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
-   }
+        requestServ.sendRequest('post', 'setPostStatus', {
+            id:     id,
+			status: status
+        }, cb);
+	}
 });
 
 
@@ -335,103 +207,56 @@ moneyApp.service('categoriesServ', function(requestServ){
 
 
 
-moneyApp.service('accountsServ', function($http, localStorageService){
-    var token = localStorageService.get('token');
-   this.getAccounts = function(cb){
-        $http.get(config.api + '?action=getAccounts&token=' + token)
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+moneyApp.service('accountsServ', function(requestServ){
+    this.getAccounts = function(cb){
+        requestServ.sendRequest('get', 'getAccounts', {}, cb);
     }
-   this.getAccount = function(id, cb){
-        $http.get(config.api + '?action=getAccount&token=' + token + '&id=' + id)
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+    this.getAccount = function(id, cb){
+        requestServ.sendRequest('get', 'getAccount', {
+            id: id
+        }, cb);
     }
     this.editAccount = function(account, cb){
-       $http.post(config.api + '?token=' + token, {
-           action: 'editAccount',
-            id: account.id,
-            title: account.title,
+        requestServ.sendRequest('post', 'editAccount', {
+            id:      account.id,
+			title:   account.title,
             balance: account.balance,
-            panel: account.panel
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
-   }
-   this.delAccount = function(id, cb){
-       $http.post(config.api + '?token=' + token, {
-           action: 'delAccount',
-           id: id
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+            panel:   account.panel
+        }, cb);
+	}
+    this.delAccount = function(id, cb){
+        requestServ.sendRequest('post', 'delAccount', {
+			id: id
+        }, cb);
     }
 });
 
 
 
-moneyApp.service('budgetsServ', function($http, localStorageService){
-    var token = localStorageService.get('token');
-   this.getBudget = function(budget, cb){
-        $http.get(config.api + '?action=getBudget&token=' + token + '&month=' + budget.month + '&year=' + budget.year)
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+moneyApp.service('budgetsServ', function(requestServ){
+    this.getBudget = function(budget, cb){
+        requestServ.sendRequest('get', 'getBudget', {
+            month: budget.month,
+            year:  budget.year
+        }, cb);
     }
-   this.getCategory = function(id, cb){
-        $http.get(config.api + '?action=getBudgetCategory&token=' + token + '&id=' + id)
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+    this.getCategory = function(id, cb){
+        requestServ.sendRequest('get', 'getBudgetCategory', {
+            id: id
+        }, cb);
     }
     this.editCategory = function(category, cb){
-       $http.post(config.api + '?token=' + token, {
-           action: 'editBudgetCategory',
-            id: category.id,
-            month: category.month,
-           year: category.year,
-           category_id: category.category_id,
-           sum: category.sum
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
-   }
-   this.delCategory = function(id, cb){
-       $http.post(config.api + '?token=' + token, {
-           action: 'delBudgetCategory',
-           id: id
-       })
-       .success(function(data){
-            cb(data);
-        })
-        .error(function(error, status){
-            cb('requestError');
-        });
+        requestServ.sendRequest('post', 'editBudgetCategory', {
+            id:          category.id,
+			month:       category.month,
+			year:        category.year,
+            category_id: category.category_id,
+            sum:         category.sum
+        }, cb);
+	}
+    this.delCategory = function(id, cb){
+        requestServ.sendRequest('post', 'delBudgetCategory', {
+			id: id
+        }, cb);
     }
 });
